5657ad7f4501cbfc3f93ea3d8f44aa7327f19e56
fabio.petrucci <fabio.petrucci@onthebeach.co.uk>
2015-11-13 16:32:22 +0000

    Add sorting

--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -1,5 +1,6 @@
 defmodule Issues.GithubIssues do
   @user_agent [ { "User-agent", "fabio.petrucci@gmail.com" } ]
+  @github_url Application.get_env(:issues, :github_url)
 
   def fetch(user, project) do
     issue_url(user, project)
@@ -8,7 +9,7 @@ defmodule Issues.GithubIssues do
   end
 
   defp issue_url(user, project) do
-    "https://api.github.com/repos/#{user}/#{project}/issues"
+    "#{@github_url}/repos/#{user}/#{project}/issues"
   end
 
   defp handle_response({:ok, %HTTPoison.Response{status_code: 200, body: body}}) do
